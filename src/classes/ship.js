656786d38b08cb13e2f1a0538d96a1b665c9257a
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-02-12 04:07:35 +1030

    Create page header and gameboard
    - Created the header of the page
    
    - Created a 10x10 grid div element and populated it with 'cell' divs
    to represent the gameboard
    
    - Stylized page for an aesthetic fitting for the game

--- src/classes/ship.js
+++ src/classes/ship.js
@@ -1,5 +1,8 @@
 class Ship {
+    // Auto-incrementing ID
     static id = 0;
+
+    // Constructor
     constructor(_length = 1, _dir = "x") {
         this.id = ++this.constructor.id;
         this.length = _length,
@@ -7,11 +10,13 @@ class Ship {
         this.timesHit = 0;
     }
 
+    // Increment times hit if the ship has not sunk
     hit() {
-        if(this.timesHit < this.length) this.timesHit++;
+        if(!this.isSunk()) this.timesHit++;
         return this.timesHit;
     }
 
+    // If times hit is equal to or greater than length, it means the ship has sunk
     isSunk() {
         return this.timesHit >= this.length;
     }
